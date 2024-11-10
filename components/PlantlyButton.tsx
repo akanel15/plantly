0a6b3b695d862a80a49fc1d1c3ab80d685d162c8
Alex Kanellis <akan0033@student.monash.edu>
2024-11-10 13:47:57 +1100

    plant add form in modal

--- components/PlantlyButton.tsx
+++ components/PlantlyButton.tsx
@@ -35,6 +35,7 @@ const styles = StyleSheet.create({
     color: theme.colorWhite,
     fontSize: 18,
     fontWeight: "bold",
+    textAlign: "center",
   },
   button: {
     paddingHorizontal: 18,
